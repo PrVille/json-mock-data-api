c92566becc2b120bf5379f1ded5bfa134896ce27
ville <ville.prami@aalto.fi>
2023-09-04 14:07:50 +0300

    vercel rewrites and analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 import "./index.css"
 import { BrowserRouter as Router } from "react-router-dom"
+import { inject } from "@vercel/analytics"
+
+inject()
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
